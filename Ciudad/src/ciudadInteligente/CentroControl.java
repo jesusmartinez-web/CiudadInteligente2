8642785b8c5ef78e3941d6c09ab4da512672dc93
HP <saengermarcos2006@gmail.com>
2026-05-18 22:48:30 -0300

    implementando logica lambda

--- Ciudad/src/ciudadInteligente/CentroControl.java
+++ Ciudad/src/ciudadInteligente/CentroControl.java
@@ -29,13 +29,7 @@ public class CentroControl {
 	}
         
         public static void eliminarVehiculo(int ID){
-            for(int i=0; i<vehiculos.size();i++){
-                int tempID = vehiculos.get(i).getId();
-                if(ID == tempID){
-                    vehiculos.remove(i);
-                    break;
-                }
-            }
+           vehiculos.removeIf(v -> v.getId() == ID);
         }
         
 }
